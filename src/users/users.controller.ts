d35d2afec88d71b3bfcdc5e9712c2570dcc10791
Mario <jmario.pl15@gmail.com>
2026-06-15 20:48:00 -0600

    Si

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,17 @@
 import { Controller, Get, Query, UseGuards, Request, Param, ParseIntPipe } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { AuthGuard } from '../auth/auth.guard';
+import { RolesGuard } from '../common/guards/roles.guard';
+import { Role } from '../role.enum';
+import { Roles } from '../common/decorators/roles.decorator';
 
 @Controller('users')
 export class UsersController {
     constructor(private readonly usersService: UsersService) {}
 
     @Get('affinity')
-    @UseGuards(AuthGuard)
+    @UseGuards(AuthGuard, RolesGuard)
+    @Roles(Role.USER)
     getAffinity(@Request() req: any, @Param('id', ParseIntPipe) otherUserId: number) {
         const currentUserId = req.user.id;
         return this.usersService.getAffinity(currentUserId, otherUserId);
